38ba2a7dcf90f2be39c195c23b4a6c3a4fe1493f
rk <rafal.kowalski@mac.com>
2016-12-21 16:26:10 +0100

    Adding entity wrappers.

---
+++ ScalpsSDK/ScalpsUser.swift
@@ -0,0 +1,14 @@
+//
+//  ScalpsUser.swift
+//  Scalps
+//
+//  Created by Rafal Kowalski on 21/12/2016
+//  Copyright © 2016 Scalps. All rights reserved.
+//
+
+import Foundation
+import Scalps
+
+open class ScalpsUser: User  {
+
+}
