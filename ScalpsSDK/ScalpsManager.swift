2d03dbb02ae04978a71e7561cf4924530282aef3
rk <rafal.kowalski@mac.com>
2017-03-13 18:59:29 +0100

    Added some get method implementations.

--- ScalpsSDK/ScalpsManager.swift
+++ ScalpsSDK/ScalpsManager.swift
@@ -192,8 +192,13 @@ open class ScalpsManager: ScalpsSDK {
 
 
     func getUser(_ userId: String, completion: @escaping (_ user: User) -> Void) {
-        let userCompletion = completion
-
+        if let u = scalpsUser {
+            completion(u.user)
+        } else {
+            print("Scalps user doesn't exist!")
+            //            // throw ScalpsManagerError.userNotIntialized
+        }
+        
 //        if let u = scalpsUser {
 //            let _ = Scalps.UserAPI.getUser(userId: u.user.userId!) {
 //                (user, error) -> Void in
@@ -209,15 +214,68 @@ open class ScalpsManager: ScalpsSDK {
 //        }
     }
 
-    func getUser(completion: @escaping (_ user: User) -> Void)  {}
-    func getDevice(_ deviceId: String, completion: @escaping (_ device: Device) -> Void) {}
-    func getDevice(completion: @escaping (_ device: Device) -> Void)  {}
+    func getUser(completion: @escaping (_ user: User) -> Void)  {
+        if let u = scalpsUser {
+            completion(u.user)
+        } else {
+            print("Scalps user doesn't exist!")
+            //            // throw ScalpsManagerError.userNotIntialized
+        }
+    }
+    
+    func getDevice(_ deviceId: String, completion: @escaping (_ device: Device) -> Void) {
+
+        if let u = scalpsUser, let d = scalpsDevice {
+            let _ = Scalps.UserAPI.getDevice(userId: u.user.userId!, deviceId: d.device.deviceId!) {
+                (device, error) -> Void in
+                
+            }
+            // XXX: ignore the returned device for now
+            completion(d.device)
+        }
+    }
+    
+    func getDevice(completion: @escaping (_ device: Device) -> Void)  {
+        if let u = scalpsUser, let d = scalpsDevice {
+            let _ = Scalps.UserAPI.getDevice(userId: u.user.userId!, deviceId: d.device.deviceId!) {
+                (device, error) -> Void in
+                
+            }
+        
+            // XXX: ignore the returned device for now
+            completion(d.device)
+        }
+    }
     func getPublication(_ publicationId: String, completion: @escaping (_ publication: Publication) -> Void) {}
     func getAllPublicationsForDevice(_ deviceId: String, completion: @escaping (_ publications: [Publication]) -> Void) {}
-    func getAllPublications(completion: @escaping (_ publications: [Publication]) -> Void) {}
-    func getSubscription(_ subscriptionId: String, completion: @escaping (_ subscription: Subscription) -> Void) {}
+
+    func getAllPublications(completion: @escaping (_ publications: [Publication]) -> Void) {
+        if let u = scalpsUser, let d = scalpsDevice {
+            // let _ = Scalps.DeviceAPI.getPublications
+//                (publications, error) -> Void in
+//                
+//            }
+            
+            // XXX: ignore the returned device for now
+            completion(publications)
+        }
+    }
+    
+    func getSubscription(_ subscriptionId: String, completion: @escaping (_ subscription: Subscription) -> Void) {
+        }
+    
     func getAllSubscriptionsForDevice(_ deviceId: String, completion: @escaping (_ subscriptions: [Subscription]) -> Void) {}
-    func getAllSubscriptions(completion: @escaping (_ subscriptions: [Subscription]) -> Void)  {}
+    func getAllSubscriptions(completion: @escaping (_ subscriptions: [Subscription]) -> Void)  {
+        if let u = scalpsUser, let d = scalpsDevice {
+            // let _ = Scalps.DeviceAPI.getPublications
+            //                (publications, error) -> Void in
+            //
+            //            }
+            
+            // XXX: ignore the returned device for now
+            completion(subscriptions)
+        }
+    }
 
     public func startMonitoringMatches() {
         if let mm = matchMonitor {
