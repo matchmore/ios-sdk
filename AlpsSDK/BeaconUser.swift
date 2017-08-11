ee7c357e219141e98d7d49793d1817ef6d8118a0
Wen <jmd@wl2s-237-13.unil.ch>
2017-08-11 17:19:49 +0200

    Removed the Protocol dummy class and now is ready to use api generated class for device

--- AlpsSDK/BeaconUser.swift
+++ AlpsSDK/BeaconUser.swift
@@ -14,7 +14,7 @@ import CoreLocation
 // AlpsManager will call BeaconUser, which will communicate with the API.
 class BeaconUser:NSObject {
     var alpsManager : AlpsManager
-    var beacons = [PBeaconDevice]()
+    var beacons = [BeaconDevice]()
     
     init (alpsManager : AlpsManager) {
         self.alpsManager = alpsManager
@@ -29,13 +29,13 @@ class BeaconUser:NSObject {
 //        beacons.append(DeviceBis.init(mobileName: "mobile1", platform: "ios 9.0", deviceToken: "xxx", location: nil))
 //        beacons.append(DeviceBis.init(mobileName: "mobile2", platform: "ios 10.0", deviceToken: "xxx", location: CLLocation.init(latitude: CLLocationDegrees(46.522076), longitude: CLLocationDegrees(6.583502))))
 //        beacons.append(DeviceBis.init(pinName: "pin1", location: CLLocation.init(latitude: CLLocationDegrees(46.522076), longitude: CLLocationDegrees(6.583502))))
-        beacons.append(PBeaconDevice.init(name: "beacon1 - rose", uuid: UUID.init(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 21978, minor: 59907))
-        beacons.append(PBeaconDevice.init(name: "beacon2 - jaune canari", uuid: UUID.init(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 64575, minor: 19467))
-        beacons.append(PBeaconDevice.init(name: "beacon3 - bordeau-betrave", uuid: UUID.init(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 53494, minor: 28090))
+        beacons.append(BeaconDevice.init(name: "beacon1 - rose", uuid: UUID.init(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 21978, minor: 59907))
+        beacons.append(BeaconDevice.init(name: "beacon2 - jaune canari", uuid: UUID.init(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 64575, minor: 19467))
+        beacons.append(BeaconDevice.init(name: "beacon3 - bordeau-betrave", uuid: UUID.init(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 53494, minor: 28090))
         print("DUMMY Beacons ready.")
     }
     
-    public func getBeacons() -> [PBeaconDevice]{
+    public func getBeacons() -> [BeaconDevice]{
         return beacons
     }
 }
